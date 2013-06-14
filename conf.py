7ae313a584daca0681595c219cab26a3c56f4be0
Kozo Nishida <knishida@riken.jp>
2013-06-15 03:26:55 +0900

    added conf.py

---
+++ conf.py
@@ -0,0 +1,24 @@
+import sys, os
+
+extensions = []
+
+templates_path = ['_templates']
+
+source_suffix = '.rst'
+
+master_doc = 'index'
+
+project = 'spatiocyte'
+copyright = 'Satya Arjunan, 2013 E-Cell project'
+
+version = '1.0'
+release = '1.0'
+
+exclude_trees = ['_build']
+
+pygments_style = 'sphinx'
+
+html_theme = 'default'
+
+html_static_path = ['_static']
+
